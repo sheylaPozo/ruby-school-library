0647189ba92861754ff2a1a880d532e47ef3a4ce
eroiyo <59938389+eroiyo@users.noreply.github.com>
2021-10-11 09:53:05 -0400

    finish student

--- person.rb
+++ person.rb
@@ -1,22 +1,26 @@
+# frozen_string_literal: true
+
+# Base data than all the people share
 class Person
-    attr_accessor :name, :age
-    attr_reader :id, :rentals
-  
-    def initialize(age:, parent_permission: true, name: 'Unknown')
-      @id = Random.rand(1..1000)
-      @name = name
-      @age = age
-      @parent_permission = parent_permission
-    end
+  attr_accessor :name, :age
+  attr_reader :id, :rentals
 
-    private def is_of_age?
-      return age>= 18
-    end
+  def initialize(age:, parent_permission: true, name: 'Unknown')
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
 
-    def can_use_services?
-      if(is_of_age? || @parent_permission)
-        return true
-      end
-      return false
-    end
+  def can_use_services?
+    return true if is_of_age? || @parent_permission
+
+    false
+  end
+
+  private
+
+  def of_age?
+    age >= 18
+  end
 end
--- student.rb
+++ student.rb
@@ -1,10 +1,17 @@
+# frozen_string_literal: true
+
 require_relative 'person'
 
+# Students information and methods
 class Student < Person
-    attr_reader :classroom
-  
-    def initialize(age:, classroom:, name: 'Unknown', parent_permission: true)
-      super(name: name, age: age, parent_permission: parent_permission)
-      @classroom = classroom
-    end
+  attr_reader :classroom
+
+  def initialize(age:, classroom:, name: 'Unknown', parent_permission: true)
+    super(name: name, age: age, parent_permission: parent_permission)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
 end
