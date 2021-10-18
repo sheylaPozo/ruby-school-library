fc93f9bb7b4e589f56c2cff984df54b732710f48
eroiyo <59938389+eroiyo@users.noreply.github.com>
2021-10-18 12:53:24 -0400

    reorder create functions and option proccesor function

--- handler.rb
+++ handler.rb
@@ -78,4 +78,91 @@ module Handler
     end
   end
 
+  def cr_a_person
+    option = 'agegraegr'
+    while option != '1' && option != '2'
+      print('invalid option, please select 1 or 2') if option != 'agegraegr'
+      puts('you would like to create an student(1) or a teacher(2)?')
+      option = gets.chomp
+    end
+    print('Name: ')
+    name = gets.chomp
+    print('Age: ')
+    age = gets.chomp
+    age = age.to_i
+    if option == '1'
+      permission = 'agegraegr'
+      while permission != 'Y' && permission != 'N' && permission != 'y' && permission != 'n'
+        puts('invalid option, please select Y or N') if permission != 'agegraegr'
+        puts('have parents permissions (Y/N)?')
+        permission = gets.chomp
+      end
+      new_student = Student.new(age, 'later', name, permission)
+      $person_list.push(new_student)
+      return "Student #{new_student.name} have been created succesfully"
+    end
+    print('Specialization: ')
+    specialization = gets.chomp
+    new_teacher = Teacher.new(age, specialization, name)
+    $person_list.push(new_teacher)
+    "Teacher #{new_teacher.name} have been created succesfully"
+  end
+
+  def cr_a_book
+    print('Title: ')
+    title = gets.chomp
+    print('Author: ')
+    author = gets.chomp
+    new_book = Book.new(title, author)
+    $book_list.push(new_book)
+    "Book #{new_book.title} have been created succesfully"
+  end
+
+  def cr_a_rental
+    if $person_list.length.positive? && $book_list.length.positive?
+      ob_person = 0
+      until ob_person >= 1 && ob_person < $person_list.length + 1
+        puts('select one person in the list to create the rental, using the numbers of the left')
+        puts('')
+        display_persons
+        ob_person = gets.chomp
+        ob_person = ob_person.to_i
+      end
+      ob_person = $person_list[ob_person - 1]
+      ob_book = 0
+      Console.clean
+      until ob_book >= 1 && ob_book < $book_list.length + 1
+        puts('select one book to create the rental, using the numbers of the left')
+        puts('')
+        display_books
+        ob_book = gets.chomp
+        ob_book = ob_book.to_i
+      end
+      Console.clean
+      ob_book = $book_list[ob_book - 1]
+      puts('insert a date')
+      date = gets.chomp
+      Rental.new(date, ob_book, ob_person)
+
+      return('Rental created succesfully')
+    end
+    puts('books and persons should be registered in the system to do a rental')
+    puts('')
+    Console.continue_story
+    'books and persons should be registered in the system to do a rental'
+  end
+
+  def exit
+    'bye'
+  end
+
+  def procces_option(entry)
+    return lis_t_books if entry == '1'
+    return lis_t_persons if entry == '2'
+    return cr_a_person if entry == '3'
+    return cr_a_book if entry == '4'
+    return cr_a_rental if entry == '5'
+    return lis_t_rentals if entry == '6'
+    return exit if entry == '7'
+  end
 end
