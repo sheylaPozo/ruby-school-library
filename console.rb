a7853b11db407fddc9f5c1aa2d0b0e8c0b2cb75b
sheylaPozo <shiggipoke@gmail.com>
2021-10-18 10:54:01 -0500

    moving the console display functions

---
+++ console.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Console
+    def self.continue_story
+      print 'press any key to continue'
+  
+      $stdin.getch
+  
+      print "            \r"
+    end
+  
+    def self.clean
+      system('clear') || system('cls')
+    end
+  end
