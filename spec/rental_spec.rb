40348a7ebbb02959647746da89dced1b489c8610
sheylaPozo <shiggipoke@gmail.com>
2021-10-20 11:48:19 -0500

    add rental andd student spec files, and contain

---
+++ spec/rental_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require_relative 'spec_resume'
+
+class Test
+end
+
+describe Test do
+  it 'the rental return the date' do
+    $book_list = []
+    $person_list = []
+    mock_p = Teacher.new(50, 'Biology', 'Hector', nil)
+    $person_list.push(mock_p)
+    mock_b = Book.new('Una Muerte Anunciada', 'Gabriel Garcia Marquez')
+    $book_list.push(mock_b)
+    mock= Rental.new("31/12/1999",0,0);
+    expect(mock.date).to eq("31/12/1999")
+  end
+end
